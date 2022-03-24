adba70a1da261df5e6bf3cb22b8e9668ba62dc40
fcarq <fcarq@protonmail.com>
2022-03-24 19:24:00 +0000

    added logic to processMultiBrandWebsite, created getLogicForWebsite

--- content.js
+++ content.js
@@ -4,6 +4,7 @@
 // https://github.com/fcarq/shopWithMeaning/wiki/Type-of-websites
 const WEBSITE_TYPE_ONE_BRAND = 'WS_TYPE_ONE';
 const WEBSITE_TYPE_MULT_BRAND = 'WS_TYPE_MULT';
+const FETCHING_TYPE_BY_CLASS = 'class';
 
 // Love icon: <a href="https://www.flaticon.com/free-icons/happy-face" title="happy-face icons">Happy-face icons created by Freepik - Flaticon</a>
 /**
@@ -57,10 +58,10 @@ function checkIfBrandInfoExists() {
 }
 
 /**
- * TODO: add type of fetching in interface(byclass, byid, ??)
+ *
  * @param {*} nameToFetch
  */
-function getAllPageBrands(nameToFetch) {
+function getAllPageBrandsViaClass(nameToFetch) {
   const elements = document.getElementsByClassName(nameToFetch);
   [].map.call(elements, (element) => setBrandInfo(element));
 }
@@ -70,24 +71,40 @@ function getAllPageBrands(nameToFetch) {
  * @return {string}
  */
 function getWebsiteInfo() {
+  // todo
   return WEBSITE_TYPE_MULT_BRAND;
 }
 
 /**
  *
+ * @param {*} url
+ * @return {[string, string]}
  */
-function processMultiBrandWebsite() {
-  // fetch the logic of this specific website. for now logic is just
-  // the tags that contain the brands
-  getAllPageBrands('ct-tile--brand');
+function getLogicForWebsite(url) {
+  // todo
+  return [FETCHING_TYPE_BY_CLASS, 'ct-tile--brand'];
+}
+
+/**
+ *
+ * @param {string} url
+ */
+function processMultiBrandWebsite(url) {
+  const [typeOfElement, value] = getLogicForWebsite(url);
+
+  switch (typeOfElement) {
+    case FETCHING_TYPE_BY_CLASS:
+      getAllPageBrandsViaClass(value);
+      break;
+  }
 }
 
 /**
  *
  */
 function processPage() {
-  const tabUrl = document.URL;
-  const typeOfWebsite = getWebsiteInfo(tabUrl);
+  const url = document.URL;
+  const typeOfWebsite = getWebsiteInfo(url);
 
   switch (typeOfWebsite) {
     case WEBSITE_TYPE_ONE_BRAND:
@@ -96,7 +113,7 @@ function processPage() {
 
 
     case WEBSITE_TYPE_MULT_BRAND:
-      processMultiBrandWebsite();
+      processMultiBrandWebsite(url);
       break;
     default:
       break;
